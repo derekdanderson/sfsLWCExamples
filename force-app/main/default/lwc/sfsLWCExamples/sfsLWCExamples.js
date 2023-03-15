1c5cf05e8783c71b01447b4cf6a8e8d49a061a5f
Derek D Anderson <derek.anderson@salesforce.com>
2023-03-15 12:31:38 -0500

    update Card with recordID

--- force-app/main/default/lwc/sfsLWCExamples/sfsLWCExamples.js
+++ force-app/main/default/lwc/sfsLWCExamples/sfsLWCExamples.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from "lwc";
 
-export default class SfsLWCExamples extends LightningElement {}
+export default class SfsLWCExamples extends LightningElement {
+  @api recordId;
+}
