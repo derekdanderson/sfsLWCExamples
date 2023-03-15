7af8290fd67bff7c331d0f33094bd166b52ee94a
Derek D Anderson <derek.anderson@salesforce.com>
2023-03-15 14:46:03 -0500

    update Card with Appointment Number

--- force-app/main/default/lwc/sfsLWCExamples/sfsLWCExamples.js
+++ force-app/main/default/lwc/sfsLWCExamples/sfsLWCExamples.js
@@ -1,5 +1,19 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, wire } from "lwc";
+import { getRecord } from "lightning/uiRecordApi";
 
 export default class SfsLWCExamples extends LightningElement {
   @api recordId;
+
+  @wire(getRecord, {
+    recordId: "$recordId",
+    fields: [
+      "ServiceAppointment.AppointmentNumber"
+    ]
+  })
+  serviceAppt;
+
+  get appointmentNumber() {
+    console.log(this.serviceAppt);
+    return this.serviceAppt?.data?.fields?.AppointmentNumber?.value;
+  }
 }
