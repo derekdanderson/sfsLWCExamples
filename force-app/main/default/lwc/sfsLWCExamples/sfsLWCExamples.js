0401745d78721788a4e4a926d69d4a1bbb78802f
Derek D Anderson <derek.anderson@salesforce.com>
2023-04-03 16:04:34 -0500

    udpate Get Parent Record

--- force-app/main/default/lwc/sfsLWCExamples/sfsLWCExamples.js
+++ force-app/main/default/lwc/sfsLWCExamples/sfsLWCExamples.js
@@ -7,13 +7,34 @@ export default class SfsLWCExamples extends LightningElement {
   @wire(getRecord, {
     recordId: "$recordId",
     fields: [
-      "ServiceAppointment.AppointmentNumber"
+      "ServiceAppointment.AppointmentNumber",
+      "ServiceAppointment.Subject",
+      "ServiceAppointment.ParentRecordId"
     ]
   })
   serviceAppt;
 
-  get appointmentNumber() {
+  get apptNumber() {
     console.log(this.serviceAppt);
     return this.serviceAppt?.data?.fields?.AppointmentNumber?.value;
+  }
+
+  get apptSubject() {
+    return this.serviceAppt?.data?.fields?.Subject?.value;
+  }
+
+  get apptParRecId() {
+    return this.serviceAppt?.data?.fields?.ParentRecordId?.value;
+  }
+
+  @wire(getRecord, {
+    recordId: "$apptParRecId",
+    fields: ["WorkOrder.WorkOrderNumber", "WorkOrder.WorkTypeId"]
+  })
+  workOrder;
+
+  get woNumber() {
+    console.log(this.workOrder);
+    return this.workOrder?.data?.fields?.WorkOrderNumber?.value;
   }
 }
